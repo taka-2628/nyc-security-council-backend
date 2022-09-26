40e1451084cf5aa45fa9c1e6d13914f1e93cdcb0
Takashi <th2826@columbia.edu>
2022-09-26 11:05:48 -0400

    Camera serializer created

---
+++ app/serializers/camera_serializer.rb
@@ -0,0 +1,8 @@
+class CameraSerializer < ActiveModel::Serializer
+  attributes :id, :image_url, :geometry, :address, :intersection, :zipcode, :borough, :description, :owner
+
+  belongs_to :neighborhood
+  belongs_to :user
+  has_many :comments
+  has_many :types
+end
