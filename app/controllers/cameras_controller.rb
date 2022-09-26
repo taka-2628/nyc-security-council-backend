23ff34af39bd6ab1629a6aa6283ab66ce092fab8
Takashi <th2826@columbia.edu>
2022-09-25 22:50:27 -0400

    CamerasController #index, #show, #create, #update and #destroy actions created

--- app/controllers/cameras_controller.rb
+++ app/controllers/cameras_controller.rb
@@ -1,2 +1,54 @@
 class CamerasController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_invalid_response
+
+  def index
+    cameras = Camera.all
+    render json: cameras, include: ['user', 'neighborhood', 'comments', 'comments.user', 'types']
+  end
+
+  def show
+    camera = find_camera
+    render json: camera, include: ['user', 'neighborhood', 'comments', 'comments.user', 'types']
+  end
+
+  def create
+    camera = Camera.create!(camera_params)
+    types = params[:types]
+    
+    types.each do |type|
+      camera.camera_types.create(type_id: type[:id])
+    end
+    render json: camera, status: :created
+  end
+
+  def update
+    camera = find_camera
+    camera.update(camera_params)
+    render json: camera, status: :accepted
+  end
+
+  def delete
+    camera = find_camera
+    camera.destroy
+    head :no_content, status: :deleted
+  end
+
+  private 
+
+  def find_camera
+    Camera.find(params[:id])
+  end
+
+  def camera_params
+    params.permit(:user_id, :neighborhood_id, :image_url, :geometry, :address, :intersection, :zipcode, :borough, :description, :owner)
+  end
+
+  def render_not_found_response
+    render json: { error: "Camera not found" }, status: :not_found
+  end
+
+  def render_invalid_response(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  end
 end
