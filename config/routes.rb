3df381fa325b71d4190ccc9e89ea0d6bd05203b0
Takashi <th2826@columbia.edu>
2022-09-25 21:08:48 -0400

    Custom routes for login, signup, autologin and logout created

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,8 @@ Rails.application.routes.draw do
   resources :types, only: [:index]
   resources :social_media_platforms, only: [:index]
 
+  post '/signup', to: 'users#create'
+  get '/me', to: 'users#show'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
