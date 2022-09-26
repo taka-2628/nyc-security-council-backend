35eb1b6b477231b36f16c07584024d4a2bd6b3e6
Takashi <th2826@columbia.edu>
2022-09-25 22:35:24 -0400

    UsersController #show and #create actions created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+
+  def show
+    user = User.find_by(id: session[:user_id])
+    if user
+      render json: user, status: :ok
+    else
+      render json: { error: "Not authorize" }, status: :unauthorized
+    end
+  end
+
+  def create
+    user = User.create(user_params)
+    if user.valid?
+      session[:user_id] = user[:id]
+      render json: user, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:username, :password, :password_confirmation, :neighborhood_id, :email, :fullname, :address_1, :address_2, :city, :state, :zipcode, :data_of_birth, :gender, :occupation, :citizenship)
+  end
+
 end
