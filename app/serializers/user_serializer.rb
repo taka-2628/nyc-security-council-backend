eb18c6be4b609f631fcce7b22839fa06b42c5f78
Takashi <th2826@columbia.edu>
2022-09-26 11:00:00 -0400

    User serializer created

---
+++ app/serializers/user_serializer.rb
@@ -0,0 +1,8 @@
+class UserSerializer < ActiveModel::Serializer
+  attributes :id, :neighborhood_id, :username, :email, :fullname, :address_1, :address_2, :city, :state, :zipcode, :data_of_birth, :gender, :occupation, :citizenship
+
+  belongs_to :neighborhood
+  has_many :cameras
+  has_many :comments
+  has_many :social_media_platforms
+end
