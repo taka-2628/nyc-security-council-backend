d3e97c7a357af25db0a883db021e6d57f7f66a95
Takashi <th2826@columbia.edu>
2022-09-25 21:38:59 -0400

    Associations created in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  has_secure_password
+  
+  belongs_to :neighborhood
+  
+  has_many :cameras
+  has_many :comments
+  has_many :user_social_media_platforms
+
+  has_many :social_media_platforms, through: :user_social_media_platforms
 end
