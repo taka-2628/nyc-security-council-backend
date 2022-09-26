7eecbb03bdb948a06493544b29fb6bdbcabdcd24
Takashi <th2826@columbia.edu>
2022-09-25 21:42:25 -0400

    Associations created in Comment model

--- app/models/camera.rb
+++ app/models/camera.rb
@@ -1,2 +1,11 @@
 class Camera < ApplicationRecord
+
+  belongs_to :neighborhood
+  belongs_to :user
+
+  has_many :comments
+  has_many :camera_types
+
+  has_many :types, through: :camera_types
+
 end
