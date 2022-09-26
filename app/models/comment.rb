65de917bf4a285803ea3ee5061267b4ede131d5b
Takashi <th2826@columbia.edu>
2022-09-26 00:12:49 -0400

    validations added in Comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,8 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :camera
   
+  validates :user_id, presence: true
+  validates :camera_id, presence: true
+  validates :body, presence: true, length: { maximum: 250 }
+
 end
