90cd98cd5f22bf629af0cff3ed3376dee44b97f1
Takashi <th2826@columbia.edu>
2022-09-26 00:06:08 -0400

    validations added in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,9 @@ class User < ApplicationRecord
   has_many :user_social_media_platforms
 
   has_many :social_media_platforms, through: :user_social_media_platforms
+
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true, length: { in: 6..20 }
+  validates :email, presence: true, uniqueness: true
+
 end
