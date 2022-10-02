eae2433f122c9c45c7d0ab20a74d52f36d2f0bf9
Takashi <th2826@columbia.edu>
2022-10-02 16:21:10 -0400

    rack-cors gem enabled and react front-end url for development added as an accepted origin in cors.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,64 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_26_003546) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cameras", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "neighborhood_id"
+    t.string "image_url"
+    t.string "geometry"
+    t.string "address"
+    t.string "intersection"
+    t.string "zipcode"
+    t.string "borough"
+    t.text "description"
+    t.string "owner"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "camera_id"
+    t.string "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "neighborhoods", force: :cascade do |t|
+    t.string "neighborhood"
+  end
+
+  create_table "social_media_platforms", force: :cascade do |t|
+    t.string "social_media"
+  end
+
+  create_table "user_social_media_platforms", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "social_media_platform_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.integer "neighborhood_id"
+    t.string "username"
+    t.string "password_digest"
+    t.string "email"
+    t.string "fullname"
+    t.string "phone"
+    t.string "address_1"
+    t.string "address_2"
+    t.string "city"
+    t.string "state"
+    t.string "zipcode"
+    t.date "date_of_birth"
+    t.string "gender"
+    t.string "occupation"
+    t.string "citizenship"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
