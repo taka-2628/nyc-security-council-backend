4644ec6c8be2773a68020642dc86014dcb463bee
Takashi <th2826@columbia.edu>
2022-10-02 11:53:37 -0400

    create_types and create_camera_types migrations deleted

--- db/migrate/20220925234739_create_types.rb
+++
@@ -1,7 +0,0 @@
-class CreateTypes < ActiveRecord::Migration[7.0]
-  def change
-    create_table :types do |t|
-      t.string :type
-    end
-  end
-end
--- db/migrate/20220926003241_create_camera_types.rb
+++
@@ -1,8 +0,0 @@
-class CreateCameraTypes < ActiveRecord::Migration[7.0]
-  def change
-    create_table :camera_types do |t|
-      t.integer :camera_id
-      t.integer :type_id
-    end
-  end
-end
