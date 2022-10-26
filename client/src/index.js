14540da79fcb4ef17e84a30c3c6c068f855348ea
Takashi <th2826@columbia.edu>
2022-10-25 20:14:39 -0400

    commits reverted

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
-import { UserProvider } from './context/user';
-
-import App from './components/App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <UserProvider>
-      <App />
-    </UserProvider>
+    <App />
   </React.StrictMode>
 );
 
